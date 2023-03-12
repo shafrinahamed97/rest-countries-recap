9ce2277346419d86db28eb484238eaf5567f7b26
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-03-12 11:36:34 +0600

    template, dymically create HTML elements based on data

---
+++ js/countries.js
@@ -0,0 +1,27 @@
+const loadCountries = () => {
+  fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data));
+}
+
+const displayCountries = countries => {
+  const countriesHTML = countries.map(country => getCountryHTML(country));
+  // console.log(countriesHTML);
+  const container = document.getElementById('countries');
+  container.innerHTML = countriesHTML;
+}
+
+const getCountryHTML = country => {
+  return `
+  
+<div class= "country">
+<h2>
+${country.name.common}
+</h2>
+<img src="${country.flags.png}">
+</div>
+
+`
+}
+
+loadCountries();
